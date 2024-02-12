472671a1f7ef9e04f6ee3bdc053268b36a210f07
Khefifi Oussama <62432594+khefifioussama@users.noreply.github.com>
2024-02-12 11:39:16 +0100

    Page login & page inscription

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {PageLoginComponent} from "./pages/page-login/page-login.component";
+import {PageInscriptionComponent} from "./pages/page-inscription/page-inscription.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PageLoginComponent, PageInscriptionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
