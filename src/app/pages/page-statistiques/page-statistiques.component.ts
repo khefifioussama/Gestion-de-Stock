fabeb96f0e8cefbcf0ab64c186a2d37863a44891
Khefifi Oussama <62432594+khefifioussama@users.noreply.github.com>
2024-02-12 14:32:44 +0100

    Config Routing & create Dashboard

---
+++ src/app/pages/page-statistiques/page-statistiques.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+@Component({
+  selector: 'app-page-statistiques',
+  standalone: true,
+  imports: [],
+  templateUrl: './page-statistiques.component.html',
+  styleUrl: './page-statistiques.component.css'
+})
+export class PageStatistiquesComponent {
+
+}
